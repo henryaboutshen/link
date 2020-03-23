89a0446912cff36109cb15c6d2a55ff3fc171271
HenrySHENhr <shenhanrui@outlook.com>
2020-03-23 14:54:46 +0800

    Update after function

--- test/climate-score.js
+++ test/climate-score.js
@@ -3,6 +3,7 @@ const { generateGlobals } = require('../src/globals-generator')
 const MongoClient = require('mongodb').MongoClient;
 
 var client;
+var variables = {};
 
 before(async function () {
     client = await MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true });
@@ -10,13 +11,23 @@ before(async function () {
 
 after(function () {
     client.close();
+    generateGlobals(variables);
 })
 
 describe('#find()', function () {
     it('responds with matching records', async function () {
         let res = await client.db("atp").collection("sanity").find({}).toArray();
         console.log(res)
-        generateGlobals({ 'a': 1 })
-        assert.equal([1, 2, 3].indexOf(4), -1);
+        assert.equal(res[0]["Name"], "Case 1");
+        variables["Name"] = res[0]["Name"];
+    });
+});
+
+describe('#find()', function () {
+    it('responds with matching records', async function () {
+        let res = await client.db("atp").collection("sanity").find({}).toArray();
+        console.log(res)
+        assert.equal(res[0]["Name"], "Case 1");
+        variables["Name2"] = res[1]["Name"];
     });
 });
